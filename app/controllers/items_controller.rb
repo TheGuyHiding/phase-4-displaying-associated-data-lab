bca35a02a485aad188270813c011194f00744595
Luis Ibarra <luis9609@gmail.com>
2022-07-20 12:10:23 -0600

    commit tree 163e75c8ab1471fd117c6258b5401a0111a171f8

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,8 @@
 class ItemsController < ApplicationController
+
+    def index
+        funky = Item.all
+        render json: funky, include: :user
+    end
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,8 @@
 class UsersController < ApplicationController
+
+    def show
+        thing = User.find_by(params[:id])
+        render json: thing, include: :items
+    end
+
 end
